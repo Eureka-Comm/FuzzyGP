ecdb1de1feb2e00820efaca634cfe6017efc965a
castellanos94 <aca810@gmail>
2022-04-06 19:17:19 -0500

    clean

--- fuzzylogicgp-algorithm/src/main/java/com/castellanos94/fuzzylogicgp/algorithm/KDFLC.java
+++ fuzzylogicgp-algorithm/src/main/java/com/castellanos94/fuzzylogicgp/algorithm/KDFLC.java
@@ -259,7 +259,7 @@ public class KDFLC implements IAlgorithm {
                 iteration++;
                 // Esto se cambio antes incluia a
                 // if (wasNotChanged >= num_iter / 10 && !isTheSameGenerator) {
-                if (wasNotChanged >= (int) (num_iter * 0.5) && iteration > ((int) num_iter / 10)) {
+                if (wasNotChanged >= (int) (num_iter * 0.5) && iteration > 9) {
                     int n = 0;
                     wasNotChanged = 0;
                     int index;
